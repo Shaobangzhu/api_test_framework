76019367951417922a220c3a672ceddb7af62c02
Chaoran Lu <andyluloveusc@gmail.com>
2024-01-24 17:29:29 -0800

    response object of requests

--- labs/test_req.py
+++ labs/test_req.py
@@ -1,15 +1,21 @@
 import requests
 from common.yaml_config import GetConf
 
-# username, password = GetConf().get_username_password()
-# url = GetConf().get_url()
-# data={
-#     "user":username,
-#     "password":password
-# }
-# res=requests.post(url+"/api/user/login", json=data)
-# print(res.text)
-# print(type(res))
+username, password = GetConf().get_username_password()
+url = GetConf().get_url()
+data={
+    "user":username,
+    "password":password
+}
+res=requests.post(url+"/api/user/login", json=data)
+
+print(res.status_code)
+print(res.ok)
+print(res.text)
+print(res.json)
+print(res.elapsed)
+print(res.headers)
+print(res.url)
 
 # url = GetConf().get_url()
 # token="JWT eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0cmFkaW5nX3N5c3RlbSIsImlkIjoyNCwiaWF0IjoxNzA2MTQ0MjQ2LCJleHAiOjE3MDY3NDkwNDZ9.DCNQ6vFrYbDDpev7O6QfgdfxKlwvkBEzbhpSLcaTDpA"
@@ -19,10 +25,10 @@ from common.yaml_config import GetConf
 # res=requests.post(url+"/api/wallet/get_wallet_info", headers=headers)
 # print(res.text)
 
-url=GetConf().get_url()
-token="JWT eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0cmFkaW5nX3N5c3RlbSIsImlkIjoyNCwiaWF0IjoxNzA2MTQ0MjQ2LCJleHAiOjE3MDY3NDkwNDZ9.DCNQ6vFrYbDDpev7O6QfgdfxKlwvkBEzbhpSLcaTDpA"
-headers={
-    "token": token
-}
-res=requests.get(url+"/api/router/router_list/", headers=headers)
-print(res.text)
+# url=GetConf().get_url()
+# token="JWT eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0cmFkaW5nX3N5c3RlbSIsImlkIjoyNCwiaWF0IjoxNzA2MTQ0MjQ2LCJleHAiOjE3MDY3NDkwNDZ9.DCNQ6vFrYbDDpev7O6QfgdfxKlwvkBEzbhpSLcaTDpA"
+# headers={
+#     "token": token
+# }
+# res=requests.get(url+"/api/router/router_list/", headers=headers)
+# print(res.text)
