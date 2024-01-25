7727832b5fd47666fd2577415b13a9fbd549db54
Chaoran Lu <andyluloveusc@gmail.com>
2024-01-24 17:19:43 -0800

    Send GET request to acquire left side menu bar

--- labs/test_req.py
+++ labs/test_req.py
@@ -11,10 +11,18 @@ from common.yaml_config import GetConf
 # print(res.text)
 # print(type(res))
 
-url = GetConf().get_url()
+# url = GetConf().get_url()
+# token="JWT eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0cmFkaW5nX3N5c3RlbSIsImlkIjoyNCwiaWF0IjoxNzA2MTQ0MjQ2LCJleHAiOjE3MDY3NDkwNDZ9.DCNQ6vFrYbDDpev7O6QfgdfxKlwvkBEzbhpSLcaTDpA"
+# headers={
+#     "token": token
+# }
+# res=requests.post(url+"/api/wallet/get_wallet_info", headers=headers)
+# print(res.text)
+
+url=GetConf().get_url()
 token="JWT eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0cmFkaW5nX3N5c3RlbSIsImlkIjoyNCwiaWF0IjoxNzA2MTQ0MjQ2LCJleHAiOjE3MDY3NDkwNDZ9.DCNQ6vFrYbDDpev7O6QfgdfxKlwvkBEzbhpSLcaTDpA"
 headers={
     "token": token
 }
-res=requests.post(url+"/api/wallet/get_wallet_info", headers=headers)
+res=requests.get(url+"/api/router/router_list/", headers=headers)
 print(res.text)
