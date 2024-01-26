7278b4a3a24a521ffd03d8791c0e01471019b73c
Chaoran Lu <andyluloveusc@gmail.com>
2024-01-25 21:54:23 -0800

    rerun decorator

--- testcases/test_get_wallet_info.py
+++ testcases/test_get_wallet_info.py
@@ -3,7 +3,7 @@ import pytest
 from common.common_requests import Requests
 
 class TestApi:
-
+    @pytest.mark.flaky(reruns=5)
     def test_get_wallet_info(self, token):
         headers = {"token": token("william")}
         res = Requests(headers).post("/api/wallet/get_wallet_info")
