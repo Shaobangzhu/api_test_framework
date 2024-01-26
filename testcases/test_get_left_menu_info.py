df9f6e5c4661e29d0ba345b7e9aec6a7ef5ab7f7
Chaoran Lu <andyluloveusc@gmail.com>
2024-01-25 21:39:23 -0800

    pytest-xdist

--- testcases/test_get_left_menu_info.py
+++ testcases/test_get_left_menu_info.py
@@ -2,6 +2,6 @@ from common.common_requests import Requests
 
 class TestApi:
     def test_get_left_menu_info(self, token):
-        headers = {"token": token}
+        headers = {"token": token("william")}
         res = Requests(headers).get("/api/router/router_list")
         print(res.text)
